0e5e63e10093df69a4c06a5ad6dd45ecc00ed748
Ex-Ark <the-gameur-63@hotmail.fr>
2018-01-08 23:26:55 +0100

    added discord bot / revert to non-thread version

---
+++ discord_version.rb
@@ -0,0 +1,41 @@
+require 'json'
+require 'net/http'
+require 'uri'
+
+require 'discordrb'
+
+require_relative 'src/wf_controller'
+
+
+bot = Discordrb::Commands::CommandBot.new token: 'NDAwMDMzMjU0ODMwMzc0OTEz.DTVvnw.8JgZ5sDrftjDKfG1fBxKoeYJSBw', prefix: '!', client_id: '400033254830374913'
+
+bot.command :syndicate do |_event, *args|
+  if args.any?
+    files = []
+    bot.send_temporary_message(_event.channel.id, "*Requesting datas! Hold on #{_event.author.name} ..*\n",30)
+    p "#{bot.prefix}:syndicate #{args}"
+    args.each do |syndic|
+      files << "#{syndic}.wf"
+    end
+    ctr = WFController.new
+    ctr.load_files files
+    all_orders = ctr.query_items
+    buy_orders = OrderFilter.sort_orders_by_price(OrderFilter.filter_ingame_buyers all_orders)
+    out = ''
+    buy_orders.each do |ord|
+      out << "#{ord}\n#{ord.price>=10 ? "#{ord.create_custom_private_message}" : ''}"
+    end
+    bot.send_temporary_message(_event.channel.id, "\n*loaded in #{Time.now - _event.timestamp} seconds.*",5)
+    # Again, the return value of the block is sent to the channel
+    out
+  else
+    "*No syndicates provided\n Retry with the followings : perrin loka veil hexis suda*"
+  end
+end
+
+bot.command :ping do |_event, *args|
+ 'pong'
+end
+puts "This bot's invite URL is #{bot.invite_url}."
+puts 'Click on it to invite it to your server.'
+bot.run
