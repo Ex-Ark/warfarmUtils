ca11c18c2697de9d26cdad1a8311edf35889a2d7
Ex-Ark <hydro@112483HD010032.ikexpress.com>
2018-05-28 17:08:20 +0200

    added supported commands list && aliases

--- discord_version.rb
+++ discord_version.rb
@@ -15,8 +15,27 @@ token = File.open('discord_api_key.sd').readline
 p "token found: #{token}\n"
 
 bot = Discordrb::Commands::CommandBot.new token: token, prefix: '!', client_id: '400033254830374913'
+known_commands = {
+ help:
+  "You are currently reading this silly!",
+ ping:
+  "Test me, if i'm up i'll answer pong!", 
+ syndicate:
+  "Get all orders from warframe.market for given syndicates\n\
+   Aliases: s | search\n\
+   Usage: <syndicate1> [ <syndicate2> ]\n\
+   Get all available syndicates by calling !syndicate without params"
+}
+bot.command :help do |_event, *args|
+ out = [] 
+ out << "Here is the list all currently available commands:"
+ known_commands.each do |command,explanation|
+   out << "!#{command}             ~> *#{explanation}*"
+ end
+ out.join("\n")
+end
 
-bot.command :syndicate do |_event, *args|
+bot.command [:syndicate, :search, :s] do |_event, *args|
   if args.any?
     files = []
     bot.send_temporary_message(_event.channel.id, "*Requesting datas! Hold on #{_event.author.name} ..*\n",30)
@@ -30,7 +49,7 @@ bot.command :syndicate do |_event, *args|
     buy_orders = OrderFilter.sort_orders_by_price(OrderFilter.filter_ingame_buyers all_orders)
     out = []
     buy_orders.each do |ord|
-	  out << "#{ord}#{ord.price>=10 ? "\n#{ord.create_custom_private_message}" : ''}" unless ord.price < 7
+	  out << "#{ord}#{ord.price>=10 ? "\n#{ord.create_custom_private_message}" : ''}" unless ord.price < 9
     end
 	out = out.join("\n")
 	if out.length >= DISCORD_MAX_CHAR_PER_MESSAGE
@@ -48,9 +67,10 @@ bot.command :syndicate do |_event, *args|
   end
 end
 
-bot.command :ping do |_event, *args|
+bot.command [:ping, :pang] do |_event, *args|
  'pong'
 end
+
 puts "This bot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
 bot.run
